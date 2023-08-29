db3afe20247ef0a3855735cc54a448e4212b00bc
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-08-29 19:16:22 +0200

    colorswitch

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const startButton = document.querySelector('[data-start]')
+  const stopButton = document.querySelector('[data-stop]')
+  const body = document.body
+
+  let backgroundColorId = null
+
+  startButton.addEventListener('click', startColor)
+  stopButton.addEventListener('click', stopColor)
+
+  function startColor() {
+    startButton.disabled = true
+    stopButton.disabled = false
+    backgroundColorId = setInterval (() => {
+    const randomColor = getRandomHexColor()
+    body.style.backgroundColor = randomColor}, 1000)
+  }
+
+function stopColor() {
+    startButton.disabled = false
+    stopButton.disabled = true
+    clearInterval(backgroundColorId)
+}
