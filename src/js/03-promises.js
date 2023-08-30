5c3b5a3f5e5504b2f7a7ff4b73b2bdb5bceb4d74
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-08-30 15:20:22 +0200

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from "notiflix";
+const delayInput = document.querySelector('[name="delay"]')
+const stepInput = document.querySelector('[name="step"]')
+const amountInput = document.querySelector('[name="amount"]')
+const submitButton = document.querySelector('[type="submit"]')
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+    resolve ({position, delay})
   } else {
-    // Reject
+   reject({position, delay})
+  }
+}, delay)
+  })
+}
+
+submitButton.addEventListener('click', handleClick)
+
+function handleClick(event) {
+  event.preventDefault()
+  let delay = parseInt(delayInput)
+  let step = parseInt(stepInput.value)
+  let amount = parseInt(amountInput.value)
+  for (let i = 0; i < amount; i += 1) {
+    let position = i + 1
+    createPromise(position, delay).then(({position, delay}) => {
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`, {clickToClose: true})
+    }).catch(({position, delay}) => {
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`, {clickToClose: true})
+    })
+    delay += step
   }
 }
